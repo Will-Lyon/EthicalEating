53d54ffe7ab8c46c215bc8f2aa3b62ead2512a37
Andrew <andrewp3773@gmail.com>
2022-11-15 16:04:13 -0800

    Quinns server file

---
+++ server.js
@@ -0,0 +1,66 @@
+var path = require('path')
+var express = require('express')
+var exphbs = require('express-handlebars')
+var fs = require('f')
+
+var ingredients = require("./ingredients.json")
+var brands = require("./brands.json")
+
+const { resolveSoa } = require('dns')
+const e = require('express')
+
+var port = process.env.PORT || 8000
+var app = express()
+
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res, next) {
+    res.status(200).render('landingPage')
+})
+
+app.get('/ingredient', function(req, res, next) {
+    res.status(200).render('ingredients', {
+        ingredientList: ingredients
+    })
+})
+
+app.get('/brand', function(req, res, next){
+    res.status(200).render('brands', {
+        brandList: brands
+    })
+})
+
+app.get('/brand/:id', function(req,res, next){
+    var brandId = req.params.id;
+    if (brands[brandId]){
+        res.status(200).render('brandPage', brands[brandId])
+    }
+
+    else {
+        next()
+    }
+}
+)
+
+var punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
+app.get('/ingredient/:id', function(req, res, next){
+    var ingredientId = req.params.id;
+    if (ingredients[ingredientId]) {
+        res.status(200).render('ingredientPage', ingredients[ingredientId])
+    }
+    else {
+        next()
+    }
+})
+
+app.get("*", function(req, res, next) {
+    res.status(404).render('404', {
+        path: req.url
+    })
+})
