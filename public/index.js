2e83482aff7371147b1fd05036911010f5581774
Andrew <andrewp3773@gmail.com>
2022-12-01 16:17:01 -0800

    add ingredient and add producer functionality implemented

--- public/index.js
+++ public/index.js
@@ -32,25 +32,115 @@ document.getElementById('login-link').addEventListener('click',
     });
 */
 
+/*
+ * Add ingredient functionality
+ */
 
-//Event listeners for add ingredient, procuer, and rate popups
+document.getElementById('add-ing-button').addEventListener('click',
+function () {
+    ingModal.style.display = 'flex';  
+});
+
 var ingModal = document.getElementById('ingModal')
 
-//shamelessly stolen vvv
-document.querySelectorAll('.close-ing-modal').forEach(item => {
-    item.addEventListener('click', event => {
-        ingModal.style.display = 'none'
-        //Resets all entered data for logging in or registering 
-        var userInput = document.querySelectorAll('.ing-input')
-        for (let i = 0; i < userInput.length; i++) {
-            userInput[i].value = ''
-        }
-    })
+//on submission of new ingredient sends it to server
+document.getElementById("ingredient-submit-button").addEventListener('click', function() {
+    var ingInput = document.getElementById('ingredient-name-input').value.trim()
+    if(!ingInput) { alert("Please add an ingredient") }
+    else {
+        fetch('ingredient/addNew', {
+            method: 'POST',
+            body: JSON.stringify({         
+                Name : ingInput,
+                Producers : []
+                
+            }),
+            headers: {'Content-Type': 'application/json'}
+        }).then(function(res) {
+            if(res.status === 200) {
+                //update UI to display new input
+            } else {
+                alert("Unable to save ingredient")
+            }
+        })
+    }
+    ingModal.style.display = 'none'
+    clearIngModals()
+})
+
+/*
+ * Add producer functionality
+ */
+var prodModal = document.getElementById("prodModal")
+
+//open prod modal on click
+document.getElementById("add-producer-button").addEventListener('click', function() {
+    prodModal.style.display = 'flex'
 })
 
 
-document.getElementById('add-ing-button').addEventListener('click',
-    function () {
-        ingModal.style.display = 'flex';  
-    });
+document.getElementById('producer-submit-button').addEventListener('click', function() {
+    var prodName = document.getElementById('producer-name-input').value.trim()
+    var prodScore = document.getElementById('producer-score-input').value.trim()
+    var ingredientId = document.getElementById('ingredientId').innerText
+    if(!prodName || !prodScore) {alert("please fill all fields")}
+    else {
+
+        var prodUrl = 'ingredient/' + ingredientId + '/addNewProd'
+        fetch(prodUrl, {
+            method: 'POST',
+            body : JSON.stringify({
+                producerName: prodName,
+                numReviews : 1,
+                ethicScore : prodScore,
+                ethicScoreRaw : prodScore
+            }),
+            headers: {'Content-Type' : 'application/json'}
+        }).then(function(res) {
+            if(res.status === 200) {
+
+            } else {
+                alert("Unable to save producer")
+            }
+        })
+    }
+    prodModal.style.display = 'none'
+    clearIngModals()
+})
+
+/*
+ * Rate producer functionality
+ */
+var rateModal = document.getElementById('rateModal')
+
+document.getElementById('rate-them-button').addEventListener('click', function () {
+    rateModal.style.display = 'flex'
+})
+
+document.getElementById('rating-submit-button').addEventListener('click',function() {
+    var ratingNum = document.getElementById('rating-input')
+    var ingredientId = document.getElementById('ingredientId').innerText
+    var producerId = 
+})
+
+//close modals
+document.querySelectorAll('.close-add-modal').forEach(item => {
+    item.addEventListener('click', event => {
+        ingModal.style.display = 'none'
+        prodModal.style.display = 'none'
+        rateModal.style.display = 'none'
+        clearIngModals()
+    })
+})
+
+function clearIngModals () {
+    var userInput = document.querySelectorAll('.modal-input')
+    for (let i = 0; i < userInput.length; i++) {
+        userInput[i].value = ''
+    }
+}
+
+
+
+
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var path = require('path')
 var express = require('express')
 var exphbs = require('express-handlebars')
+var fs = require('fs')
 //var fs = require('f')  not quite sure what this is for and its causing an error
 
 var ingredients = require("./ingredients.json")
@@ -8,6 +9,7 @@ var brands = require("./brands.json")
 
 const { resolveSoa } = require('dns')
 const e = require('express')
+const { runInNewContext } = require('vm')
 
 var port = process.env.PORT || 8000
 var app = express()
@@ -50,22 +52,84 @@ app.get('/brand/:id', function (req, res, next) {
 )
 
 var punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
-
+//returns searched ingredient
 app.get('/ingredient/:id', function (req, res, next) {
     var ingredientId = req.params.id;
     
     if(ingredientId.includes("search")){
+        console.log("===1")
         search = req.query.q;
         console.log(search);
         res.status(200).render('ingredientPage', searching(search))
+        console.log("===1")
     }
     else if (ingredients[ingredientId]) {
+        console.log("===2")
         res.status(200).render('ingredientPage', ingredients[ingredientId])
+        console.log("===2")
     }
     else {
         next()
     }
+    
 })
+
+//accepts new ingredient and adds to database
+app.post('*/ingredient/addNew', function(req,res) {
+    if(req.body && req.body.Name && req.body.Producers && !ingredients[req.body.Name]) {
+        console.log("==Client sent: ", req.body)
+        
+        var newIngredient = {
+            Name : req.body.Name,
+            Producers : req.body.Producers   
+        }
+        ingredients.push(newIngredient)
+        
+
+        fs.writeFile('./ingredients2.json', JSON.stringify(ingredients, null, 2), function(err) {
+            if(err) {
+                res.status(500).send("error writing ingredient to database")
+            } else {
+                res.status(200).send("ingredient added")
+            }
+        })
+    } else {
+        res.status(400).send("requests must contain a json body with matching fields")
+    }
+})
+
+//accepts new producers and adds them to database VVV need to prevent it from allowing multiple entries of same brand VVV
+app.post('*/ingredient/:id/addNewProd', function(req,res) {
+    var ingredientId = req.params.id
+    var indexOf = ingredients.findIndex(i=>i.Name === ingredientId)
+    
+    if(req.body && req.body.producerName && req.body.numReviews && req.body.ethicScore && req.body.ethicScoreRaw ) {
+        console.log("==Client sent: ", req.body)
+        var newProd = {
+            producerName : req.body.producerName,
+            numReviews : req.body.numReviews,
+            ethicScore : req.body.ethicScore,
+            ethicScoreRaw : req.body.ethicScoreRaw
+        }
+        
+        ingredients[indexOf].Producers.push(newProd)
+
+        fs.writeFile('./ingredients.json', JSON.stringify(ingredients, null, 2), function(err) {
+            if(err) {
+                res.status(500).send("error writing ingredient to database")
+            } else {
+                res.status(200).send("ingredient added")
+            }
+        })
+    } else {
+        res.status(400).send("requests must contain a json body with matching fields")
+    }
+})
+
+
+
+//accepts new ingredient rating and adds it to database
+//app.post()
 
 app.get("*", function (req, res, next) {
     res.status(404).render('404', {
@@ -86,5 +150,5 @@ function searching(s){
             return ingredients[i];
         }
     }
-    return {"Name": s }; 
+    return {"searchq": s }; 
 }  
